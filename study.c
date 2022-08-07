7195838af53db63ed1201c42d2fc3f5d618e0d04
Pedro Akira <pedro-akira@hotmail.com>
2022-08-07 13:31:50 -0300

    add some books about C language

---
+++ study.c
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+REFERENCE: 
+
+    https://stringfixer.com/pt/ISO_C%2B%2B
+    https://developerinsider.co/difference-between-c-and-ansi-c/
+    https://en.wikipedia.org/wiki/ANSI_C
+    https://www.tiobe.com/tiobe-index/
+    https://www.iso.org/standard/74528.html
+    https://www.open-std.org/jtc1/sc22/wg14/
+    https://developerinsider.co/difference-between-c-and-ansi-c/
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+//END
